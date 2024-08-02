8f974a009212b3cc2ea6cab9191d4c2e7e7ed435
Björn Büttner <Idrinth@users.noreply.github.com>
2024-08-02 18:31:25 +0200

    v1.0.13
    fixes #0

--- bin/iab.js
+++ bin/iab.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
 import cli from '../src/cli/cli.js';
+import {
+  config,
+} from 'dotenv';
+import {
+  existsSync,
+} from 'fs';
+
+if (existsSync(process.cwd() + '/.env')) {
+  config();
+}
 
 process.exit(await cli(process.argv, process.cwd(),),);
